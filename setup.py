172b60096cfe75e811f20f04acfd0d35dba84181
Subhan Nizar <subhannizar25@gmail.com>
2019-02-18 04:27:05 +0800

    Fixs: 1.Fixing error install requires in setup.py 2. Add requirements.txt file

--- setup.py
+++ setup.py
@@ -5,19 +5,21 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Flask2Neo4J",
-    version="0.0.1",
+    version="0.1a1",
     author="Ganggas95",
     author_email="subhannizar25@amail.com",
     description="""Extension Flask for integration with neo4j graph database""",
     long_description=long_description,
-    include_package_data=True,
+    license="MIT",
     long_description_content_type="text/markdown",
     url="https://github.com/ganggas95/flask2neo4j",
     packages=setuptools.find_packages(),
+    py_module=["flask2neo4j"],
+    include_package_data=True,
     install_requires=[
-        'Flask>=1.0',
-        'py2neo>=3.0',
-        'prompt-toolkit>=2.0.7<2.1'
+        'Flask >= 1.0',
+        'py2neo >= 3.0',
+        "prompt_toolkit<2.1,>=2.0.7"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
